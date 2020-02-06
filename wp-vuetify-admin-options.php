119cb082661fc8c19c6ab97ff7ab1de627855d65
Yair Levy <supraniti.inbox@gmail.com>
2020-02-06 17:19:58 +0200

    Create wp-vuetify-admin-options.php

---
+++ wp-vuetify-admin-options.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: wp-vuetify-admin-options
+ * Plugin URI: https://github.com/supraniti/wp-vuetify-admin-options/
+ * Description: Auto Warehouse Integration
+ * Version: 1.0.0
+ * Author: Supraniti
+ * Author URI: https://github.com/supraniti/
+ * License: GPL2
+ */
+ 
